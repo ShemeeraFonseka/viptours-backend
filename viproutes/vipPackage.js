605be7ea7de7be0de46f91cf712bf269ede945a1
Shemeera Fonseka <shemeerafonseka@gmail.com>
2025-12-16 12:48:28 +0530

    packages changed

--- viproutes/vipPackage.js
+++ viproutes/vipPackage.js
@@ -2,7 +2,7 @@ import express from 'express';
 import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
-import Vipvippackage from '../vipmodels/Vippackage.js';
+import Vipvippackage from '../vipmodels/VipPackage.js';
 
 const router = express.Router();
 
